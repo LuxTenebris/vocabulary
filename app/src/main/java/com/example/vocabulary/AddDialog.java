0ec0b099307b86e0be6e69da5a75d261be3b7115
Talancev <mishatalancev@gmail.com>
2020-09-11 23:13:20 +0300

    push_data_to_firebase

--- app/src/main/java/com/example/vocabulary/AddDialog.java
+++ app/src/main/java/com/example/vocabulary/AddDialog.java
@@ -3,6 +3,7 @@ package com.example.vocabulary;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +15,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddDialog extends DialogFragment implements View.OnClickListener {
     EditText english;
     EditText russian;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    Map<String, Object> note = new HashMap<>();
+
     @NonNull
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,8 +52,34 @@ public class AddDialog extends DialogFragment implements View.OnClickListener {
             case R.id.add_word:
                 String st_english = english.getText().toString();
                 String st_russian = russian.getText().toString();
+                if(st_english.length() == 0){
+                    Toast.makeText(getContext(),"You do not enter an english word",Toast.LENGTH_SHORT).show();
+                } else {
+                    if(st_russian.length() == 0){
+                        Toast.makeText(getContext(),"You do not enter an russian word",Toast.LENGTH_SHORT).show();
+                    } else {
 
-                Toast.makeText(getContext(),st_english + st_russian,Toast.LENGTH_SHORT).show();
+                        note.put("english", st_english);
+                        note.put("russian", st_russian);
+                        db.collection("Words")
+                                .add(note)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d("Tag", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("Tag", "Error adding document", e);
+                                    }
+                                });
+
+                        Toast.makeText(getContext(),"successfully added",Toast.LENGTH_SHORT).show();
+                        getDialog().cancel();
+                    }
+                }
                 break;
 
             case R.id.cansel_adding:
