3460f3def12c446d3fc6086bffa8b416c08dec72
Talancev <mishatalancev@gmail.com>
2020-09-17 16:43:51 +0300

    add_pull_data_from_db

--- app/src/main/java/com/example/vocabulary/WordsActivity.java
+++ app/src/main/java/com/example/vocabulary/WordsActivity.java
@@ -1,14 +1,51 @@
 package com.example.vocabulary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WordsActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    List<Word> words = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words);
+        db.collection("Words")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                words.add(new Word( (String) document.get("english"), (String) document.get("russian")));
+
+                                Log.d("TAG", document.getId() + " => " + document.get("name"));
+                            }
+
+                        }
+                        ListView wordsList = findViewById(R.id.words_list);
+
+                        wordsList.setAdapter(new WordAdapter(WordsActivity.this, words));
+                    }
+                });
+
+
     }
 }
